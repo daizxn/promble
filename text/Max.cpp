e508f51917b41fb8aef5fe91064a4f268e2dc667
daizxn <daizxn@outlook.com>
2023-08-12 16:54:45 +0800

    text

---
+++ text/Max.cpp
@@ -0,0 +1,3 @@
+int MAX(int a,int b){
+    return a>b ? a:b;
+}
---
+++ text/text.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include "Max.h"
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    cout<<n<<m;
+    cout<<MAX(n,m);
+    cout<<"一"<<endl;
+    system("pause");
+    return 0;
+}
