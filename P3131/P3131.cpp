99b21280bdcc3f622d3df6c83f15aab502191368
daizxn <daizxn@outlook.com>
2023-09-20 22:03:51 +0800

    9_20

---
+++ P3131/P3131.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int l[7] = {0, -1, -1, -1, -1, -1, -1}, r[7];
+int S = 0;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int a;
+        cin >> a;
+        S = (S + a) % 7;
+        if (l[S] == -1)
+            l[S] = i;
+        r[S] = i;
+    }
+    int ans = 0;
+    for (int i = 0; i < 7; i++)
+        if (l[i] != -1)
+            ans = max(ans, r[i] - l[i]);
+    cout << ans << endl;
+    system("pause");
+    return 0;
+}
